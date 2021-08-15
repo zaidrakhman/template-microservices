ced87d71d1432e93478e6643bbeae8a6ac11cc3a
onicq <miras.zaidrakhman@gmail.com>
2021-08-15 22:15:28 +0600

    event bus

---
+++ EventBus/Abstractions/IIntegrationEventHandler.cs
@@ -0,0 +1,12 @@
+﻿using EventBus.Events;
+using System.Threading.Tasks;
+
+namespace EventBus.Abstractions
+{
+    public interface IIntegrationEventHandler<in TIntegrationEvent> : IIntegrationEventHandler
+        where TIntegrationEvent : IntegrationEvent
+    {
+        Task Handle(TIntegrationEvent @event);
+    }
+    public interface IIntegrationEventHandler { }
+}
